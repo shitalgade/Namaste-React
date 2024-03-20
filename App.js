71d1a8ea2b973c391086b92b75a048eda04c70c7
shitalgade <shitalgade9881@gmail.com>
2024-03-20 16:59:17 +0530

    episode-1

---
+++ App.js
@@ -0,0 +1,4 @@
+    const heading1 = React.createElement("h1",{},"Hello React!!");
+    console.log(heading1);
+    const root1 = ReactDOM.createRoot(document.getElementById("root"));
+    root1.render(heading1);
